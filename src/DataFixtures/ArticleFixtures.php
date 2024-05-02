1da8d85fa5d5667d02a0b2fb8cc923442657eea3
Mezsan <florian.debosz@outlook.fr>
2024-05-02 18:39:57 +0200

    ✨  Added Commentary &  Category features

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,20 +5,54 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
+
+
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for($i = 1; $i <= 10; $i++){
-            $article = new Article();
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("<p>Contenu de l'article n°$i</p>")
-                    ->setImage("http://placehold.it/350x150")
-                    ->setCreatedAt(new \DateTime());
+        $faker = \Faker\Factory::create('fr_FR');
 
-            $manager->persist($article);
-        }           
+        for ($i = 1; $i <= 3; $i++) {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            for ($j = 1; $j <= mt_rand(4,6); $j++) {
+                $content = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
+
+                $article = new Article();
+                $article->setTitle($faker->sentence())
+                    ->setContent($content)
+                    ->setImage($faker->imageUrl())
+                    ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                    ->setCategory($category);
+
+                $manager->persist($article);
+
+                for($k = 1; $k <= mt_rand(4,10); $k++){
+                    $date = new \DateTime();
+                    $interval = $date->diff($article->getCreatedAt());
+                    $days = $interval->days;
+                    $minimumDays = '-' . $days . ' days';
+                    
+                    $content = '<p>' . join( '</p><p>' ,$faker->paragraphs(2)) . '</p>';     
+
+                    $comment = new Comment();
+                    $comment->setAuthor($faker->name())
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween($minimumDays))
+                            ->setArticle($article);
+
+                    $manager->persist($comment);
+                }
+            }
+        }
 
         $manager->flush();
     }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -38,6 +40,21 @@ class Article
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
+    /**
+     * @var Collection<int, Comment>
+     */
+    #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'article', orphanRemoval: true)]
+    private Collection $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -88,6 +105,48 @@ class Article
     public function setCreatedAt(\DateTimeInterface $createdAt): static
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getArticle() === $this) {
+                $comment->setArticle(null);
+            }
+        }
 
         return $this;
     }
